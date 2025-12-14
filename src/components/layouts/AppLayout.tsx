52ab841560f2b2881f42f38164a1b6064213356b
Abero-dev <abel04.mata@gmail.com>
2025-12-14 16:11:22 -0500

    Updating AppLayout with loading component

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -1,8 +1,16 @@
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
+import { useUser } from '@clerk/clerk-react'
+import { BarLoader } from 'react-spinners';
 
 function AppLayout() {
+
+    const { isLoaded } = useUser();
+
+    if (!isLoaded)
+        return <BarLoader className='mb-4' width={"100%"} color="#36d7b7" />
+
     return (
         <div>
             <div className='grid-background'></div>
