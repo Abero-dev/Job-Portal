7bdcfff5b2c8be129edba23bb390c758e7884ca4
Abero-dev <abel04.mata@gmail.com>
2025-12-27 16:30:38 -0500

    Adding company insert function

--- src/api/apiCompanies.ts
+++ src/api/apiCompanies.ts
@@ -14,3 +14,25 @@ export async function getAllCompanies(
 
     return data;
 }
+
+export async function addNewCompany(
+    supabase: SupabaseClient,
+    params: { company_data: any }
+) {
+    const { company_data } = params;
+
+    const { data, error } = await supabase
+        .from("companies")
+        .insert([company_data])
+        .select()
+
+    if (error) {
+        toast.error("An error occurred adding a new company. Please try again later.");
+        console.error("Insert error:", error);
+        return null;
+    }
+
+    toast.success("Company added correctly.");
+
+    return data;
+}
