a56ccdccee5adb757f62aaee6f26bc93ca49616d
Abero-dev <abel04.mata@gmail.com>
2025-12-30 19:11:29 -0500

    Adding saved jobs list component

--- src/pages/SavedJob.tsx
+++ src/pages/SavedJob.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import { getSavedJobs } from '@/api/apiJobs'
+import JobCard from '@/components/jobs/JobCard';
+import { useJobs } from '@/hooks/useJobs'
+import { useUser } from '@clerk/clerk-react';
+import { useEffect } from 'react'
+import { BarLoader } from 'react-spinners';
 
 function SavedJob() {
+
+  const { isLoaded } = useUser();
+
+  const {
+    loading: loadingSavedJobs,
+    error: errorSavedJobs,
+    data: dataSavedJobs,
+    fn: fnSavedJobs
+  } = useJobs(getSavedJobs)
+
+  useEffect(() => {
+    if (isLoaded) fnSavedJobs();
+  }, [isLoaded])
+
+  if (!isLoaded || loadingSavedJobs) return <BarLoader width={"100%"} color='#36d7b7' />
+
   return (
-    <div>SavedJob</div>
+    <div className='lg:px-40 px-4 flex flex-col items-center gap-8 pt-8'>
+      <h1 className='gradient-title font-extrabold text-6xl'>Saved Jobs</h1>
+      <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4'>
+        {
+          dataSavedJobs?.length ?
+            dataSavedJobs.map((savedJob: any) =>
+              <JobCard
+                key={savedJob.id}
+                job={savedJob?.job}
+                savedInit={true}
+                onJobSaved={fnSavedJobs}
+                isMyJob={undefined}
+              />
+            ) :
+            <div>No saved jobs found</div>
+        }
+      </div>
+    </div>
   )
 }
 
